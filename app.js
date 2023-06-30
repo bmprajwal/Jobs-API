5d7a5847d91eebd6dd3863900f58756b08b3d0ca
BM Prajwal <prajwalbm2003@gmail.com>
2023-06-30 12:34:12 +0530

    initial-setup

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/notFound')
+const errorHandlerMiddleware = require('./middleware/errorHandler')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('jobs api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
